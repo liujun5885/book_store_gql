88d58e608f6ca65d67fe513619c2b712dcbbb175
junliu <junliu@appannie.com>
2021-02-09 11:08:46 +0800

    update

--- graph/resolver/resolver.go
+++ graph/resolver/resolver.go
@@ -1,7 +1,56 @@
 package resolver
 
+import (
+	"context"
+	"fmt"
+	"github.com/liujun5885/book_store_gql/db/dborm"
+	"github.com/liujun5885/book_store_gql/graph/model"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	ORMBooks     dborm.Book
+	ORMAuthor    dborm.Author
+	ORMPublisher dborm.Publisher
+	ORMUser      dborm.User
+	ORMTopic     dborm.Topic
+}
+
+func (r *authorResolver) Books(ctx context.Context, obj *model.Author) ([]*model.Book, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *bookResolver) Authors(ctx context.Context, obj *model.Book) ([]*model.Author, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *bookResolver) Publishers(ctx context.Context, obj *model.Book) ([]*model.Publisher, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) Register(ctx context.Context, input model.RegisterInput) (*model.RegisterResponse, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) Login(ctx context.Context, input model.LoginInput) (*model.LoginResponse, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *publisherResolver) Books(ctx context.Context, obj *model.Publisher) ([]*model.Book, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) SearchBooks(ctx context.Context, keyword string) ([]*model.Book, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) FetchCurrentUser(ctx context.Context) (*model.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *topicResolver) Books(ctx context.Context, obj *model.Topic) ([]*model.Book, error) {
+	panic(fmt.Errorf("not implemented"))
+}
--- graph/resolver/schema.resolvers.go
+++ graph/resolver/schema.resolvers.go
@@ -4,48 +4,8 @@ package resolver
 // will be copied through when generating and any unknown code will be moved to the end.
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/liujun5885/book_store_gql/graph/generated"
-	"github.com/liujun5885/book_store_gql/graph/model"
 )
-
-func (r *authorResolver) Books(ctx context.Context, obj *model.Author) ([]*model.Book, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *bookResolver) Authors(ctx context.Context, obj *model.Book) ([]*model.Author, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *bookResolver) Publishers(ctx context.Context, obj *model.Book) ([]*model.Publisher, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *mutationResolver) Register(ctx context.Context, input model.RegisterInput) (*model.RegisterResponse, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *mutationResolver) Login(ctx context.Context, input model.LoginInput) (*model.LoginResponse, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *publisherResolver) Books(ctx context.Context, obj *model.Publisher) ([]*model.Book, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *queryResolver) SearchBooks(ctx context.Context, keyword string) ([]*model.Book, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *queryResolver) FetchCurrentUser(ctx context.Context) (*model.User, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *topicResolver) Books(ctx context.Context, obj *model.Topic) ([]*model.Book, error) {
-	panic(fmt.Errorf("not implemented"))
-}
 
 // Author returns generated.AuthorResolver implementation.
 func (r *Resolver) Author() generated.AuthorResolver { return &authorResolver{r} }
