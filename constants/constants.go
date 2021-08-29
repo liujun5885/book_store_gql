10209957e690b33f89a9c382d81b4050ede19127
liujun <liujun@rxthinking.com>
2021-08-29 14:19:35 +0800

    update

--- constants/constants.go
+++ constants/constants.go
@@ -2,4 +2,11 @@ package constants
 
 const (
 	JWTSecret = "JWT_SECRET"
+	// book-store-service
+	S3AccessKey    = "AKIA6DMM7NYOWQIKXNEC"
+	S3AssessSecret = "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"
+
+	// admin
+	//S3AccessKey    = "AKIA6DMM7NYO2TZUE3F3"
+	//S3AssessSecret = "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"
 )
